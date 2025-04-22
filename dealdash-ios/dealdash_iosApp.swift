5b5da08de7f747fa0c7ef6c68bcb939e0c78be0e
Gevindu-Piyumal <gevindu.personal@gmail.com>
2025-04-22 22:39:14 +0530

    Initial Commit

---
+++ dealdash-ios/dealdash_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dealdash_iosApp.swift
+//  dealdash-ios
+//
+//  Created by Gevindu Piyumal on 2025-04-22.
+//
+
+import SwiftUI
+
+@main
+struct dealdash_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
